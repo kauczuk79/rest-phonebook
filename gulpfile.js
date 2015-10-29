11b501578503fb10c0138289ff648c15cd434e79
Robert Wesołowski <kauczuk79@gmail.com>
2015-10-29 12:36:55 +0100

    Test task added, problem with shutdown node server

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,12 @@
     'use strict';
 
     var gulp = require('gulp'),
+        gulpProtractorAngular = require('gulp-angular-protractor'),
+        server = require('gulp-develop-server'),
         karma = require('gulp-karma'),
         runSequence = require('run-sequence'),
-        gulpProtractorAngular = require('gulp-angular-protractor');
+        shell = require('gulp-shell'),
+        fs = require('fs');
 
     // Setting up the protractor task 
     gulp.task('protractor', function (callback) {
@@ -29,8 +32,23 @@
         }).on('end', callback);
     });
 
+    gulp.task('start-mongo', shell.task('mongod'));
+    gulp.task('stop-mongo', shell.task('mongo admin --eval "db.shutdownServer();"'));
+
+    gulp.task('start-server', function (callback) {
+        server.listen({
+            path: './index.js',
+            killSignal: 'SIGTERM'
+        });
+    });
+
+    gulp.task('stop-server', function (callback) {
+
+        gulp.watch(['./index.js'], server.restart);
+    });
+
     //Setting up the test task to run sequence of tests
     gulp.task('test', function (callback) {
-        runSequence('karma', 'protractor', callback);
+        runSequence('karma', ['start-mongo', 'start-server', 'protractor'], 'stop-mongo', callback);
     });
 }());
